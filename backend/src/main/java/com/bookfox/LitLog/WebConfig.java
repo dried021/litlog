20e1d6c29b8eefbce9b72debbea50fd039f7d5ab
dried021 <dried021@gmail.com>
2025-04-30 17:50:40 +0900

    [04.30] 스크립트 테스트

---
+++ backend/src/main/java/com/bookfox/LitLog/WebConfig.java
@@ -0,0 +1,37 @@
+// package com.bookfox.LitLog;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.lang.NonNull;
+// import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// import com.bookfox.interceptor.SessionInterceptor;
+
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
+
+//     private static final String[] EXCLUDE_PATHS = {
+//         "/login",
+//         "/auth/**",
+//         "/api/main/getTermsList",
+//         "/resources/**",
+//         "/css/**",
+//         "/js/**",
+//         "/images/**",
+//         "/favicon.ico",
+//         "/api/login",
+//         "/api/auth/**"
+//     };
+
+//     @Autowired
+//     private SessionInterceptor sessionInterceptor;
+
+//     @Override
+//     public void addInterceptors(@NonNull InterceptorRegistry registry) {
+//         registry.addInterceptor(sessionInterceptor)
+//         .order(1)
+//         .addPathPatterns("/**")
+//         .excludePathPatterns(EXCLUDE_PATHS);
+//     }
+// }
