560b68120e517882b7f5927a3eda3276152878fb
dried021 <dried021@gmail.com>
2025-04-30 17:58:57 +0900

    [04.30] 테스트

--- backend/src/main/java/com/bookfox/LitLog/WebConfig.java
+++ backend/src/main/java/com/bookfox/LitLog/WebConfig.java
@@ -1,5 +1,5 @@
 // package com.bookfox.LitLog;
-
+//테스트
 // import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.context.annotation.Configuration;
 // import org.springframework.lang.NonNull;
