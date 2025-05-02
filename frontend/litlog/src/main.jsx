aff525d7ea40d2f94e103f21148f98b9bad76315
dried021 <dried021@gmail.com>
2025-05-02 20:07:58 +0900

    [05.02] 으악

--- frontend/litlog/src/main.jsx
+++ frontend/litlog/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 import App from './App.jsx';
 import './styles/global.css';
 //import './index.css'
