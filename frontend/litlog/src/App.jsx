e725224d0d176099821319a445d051edf447b81d
dried021 <dried021@gmail.com>
2025-05-02 19:59:14 +0900

    [05.02] 제발

--- frontend/litlog/src/App.jsx
+++ frontend/litlog/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import AppRouter from './routes/AppRouter';
-import { useState, useEffect } from 'react'
-import axios from 'axios';
 
 function App(){
   return(
@@ -19,27 +17,4 @@ function App(){
   );
 
 }
-export default App
-//api 호출 예시
-/*
-function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(()=>{
-    axios.get("http://localhost:8080/api/message")
-      .then(res=>setMessage(res.data))
-      .catch(err=> console.error("API 호출 에러: ", err));
-  }, []);
-
-  return (
-    <>
-        <div>
-          <h1>React + Spring Boot 연동</h1>
-          <p>서버 메시지: {message}</p>
-        </div>
-    </>
-  )
-}
-
-export default App
-*/
+export default App;
