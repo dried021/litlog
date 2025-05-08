01a96da300a20cb80a1eb054b3cdfc1ea900aab3
Seunghyun Nam <shyunam@gmail.com>
2025-05-09 03:48:55 +0900

    [05.09] 프로필 보기 추가

--- frontend/litlog/src/pages/Mypage/Profile.jsx
+++ frontend/litlog/src/pages/Mypage/Profile.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProfileSummary from '../../components/Profile/ProfileSummary';
 
 const Profile = () => {
     return(
         <div>
             <h2> 마이 페이지 - 프로필 </h2>
+            <ProfileSummary/>
         </div>
     );
 };
