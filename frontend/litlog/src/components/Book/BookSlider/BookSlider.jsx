0b9335b186a006896c9796ca393ca59297053e8d
dried021 <dried021@gmail.com>
2025-05-08 14:54:01 +0900

    [05.08] slider 완성

--- frontend/litlog/src/components/Book/BookSlider/BookSlider.jsx
+++ frontend/litlog/src/components/Book/BookSlider/BookSlider.jsx
@@ -2,75 +2,82 @@ import React from 'react';
 import Slider from 'react-slick';
 import { Link } from 'react-router-dom';
 import './BookSlider.css';
+function NextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        onClick={onClick}
+        style={{
+          ...style,
+          display: 'block',
+          background: `url('/arrows/arrow_forward_ios.svg') no-repeat center center`,
+          backgroundSize: 'contain',
+          width: '40px',
+          height: '40px',
+          right: '-45px',
+          zIndex: 1,
+        }}
+      />
+    );
+  }
+  
+  function PrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        onClick={onClick}
+        style={{
+          ...style,
+          display: 'block',
+          background: `url('/arrows/arrow_back_ios.svg') no-repeat center center`,
+          backgroundSize: 'contain',
+          width: '40px',
+          height: '40px',
+          left: '-45px',
+          zIndex: 1,
+        }}
+      />
+    );
+  }
 
-const books = [
-  {
-    id: 1,
-    title: 'THUNDERBOLTS',
-    image: '/images/cat.jpg',
-    link: '/books/thunderbolts',
-    views: '473K',
-    likes: '118K',
-    hearts: '205K',
-  },
-  {
-    id: 2,
-    title: 'SINNERS',
-    image: '/images/cat.jpg',
-    link: '/books/sinners',
-    views: '994K',
-    likes: '169K',
-    hearts: '468K',
-  },
-  {
-    id: 3,
-    title: 'CONCLAVE',
-    image: '/images/cat.jpg',
-    link: '/books/conclave',
-    views: '1.5M',
-    likes: '241K',
-    hearts: '498K',
-  },
-  {
-    id: 4,
-    title: 'ANOTHER SIMPLE FAVOR',
-    image: '/images/cat.jpg',
-    link: '/books/another',
-    views: '67K',
-    likes: '9.2K',
-    hearts: '9.4K',
-  },
-  // 필요한 만큼 추가 가능
-];
-
-function BookSlider() {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    arrows: true,
-  };
-
-  return (
-    <div className="book-slider">
-      <h3>POPULAR FILMS THIS WEEK</h3>
-      <Slider {...settings}>
-        {books.map((book) => (
-          <div className="book-card" key={book.id}>
-            <Link to={book.link}>
-              <img src={book.image} alt={book.title} className="book-image" />
-              <h5>{book.title}</h5>
-              <div className="book-stats">
-                👁 {book.views} &nbsp;&nbsp; 👍 {book.likes} &nbsp;&nbsp; ❤️ {book.hearts}
-              </div>
-            </Link>
-          </div>
-        ))}
-      </Slider>
-    </div>
-  );
-}
+  function BookSlider({
+    title,
+    books,
+    slidesToShow = 5,
+    slidesToScroll = 4
+  }) {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow,
+      slidesToScroll,
+      arrows: true,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />
+    };
+  
+    return (
+      <div className="book-slider">
+        {title && <h3>{title}</h3>}
+        <Slider {...settings}>
+          {books.map((book) => (
+            <div className="book-card" key={book.id}>
+              <Link to={book.link}>
+                <img src={book.image} alt={book.title} className="book-image" />
+                <h5>{book.title}</h5>
+                <div className="book-stats">
+                  👁 {book.views} &nbsp;&nbsp; 👍 {book.likes} &nbsp;&nbsp; ❤️ {book.hearts}
+                </div>
+              </Link>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    );
+  }
+  
 
 export default BookSlider;
--- frontend/litlog/src/pages/Books/BookMain.jsx
+++ frontend/litlog/src/pages/Books/BookMain.jsx
@@ -3,12 +3,54 @@ import BookSearch from "../../components/Book/BookSearch/BookSearch";
 import BookSlider from "../../components/Book/BookSlider/BookSlider";
 
 const BookMain = () => {
+
+    //테스트용
+    const books = [
+        {
+        id: 1,
+        title: 'THUNDERBOLTS',
+        image: '/images/cat.jpg',
+        link: '/books/thunderbolts',
+        views: '473K',
+        likes: '118K',
+        hearts: '205K',
+        },
+        {
+        id: 2,
+        title: 'SINNERS',
+        image: '/images/cat.jpg',
+        link: '/books/sinners',
+        views: '994K',
+        likes: '169K',
+        hearts: '468K',
+        },
+        {
+        id: 3,
+        title: 'CONCLAVE',
+        image: '/images/cat.jpg',
+        link: '/books/conclave',
+        views: '1.5M',
+        likes: '241K',
+        hearts: '498K',
+        },
+        {
+        id: 4,
+        title: 'ANOTHER SIMPLE FAVOR',
+        image: '/images/cat.jpg',
+        link: '/books/another',
+        views: '67K',
+        likes: '9.2K',
+        hearts: '9.4K',
+        },
+        // 필요한 만큼 추가 가능
+    ];
+
     return(
         <div>
             <h2> Books 메인 페이지 </h2>
-
-            <BookSlider/>
             <BookSearch/>
+            <BookSlider title="Popular Books This Week" books={books} />
+            <BookSlider title="Just Reviewed Books" books={books} />
         </div>
     );
 };
