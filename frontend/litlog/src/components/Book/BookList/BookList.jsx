d148997f0e90e37d9632a81e878d4a510f856e25
dried021 <dried021@gmail.com>
2025-05-09 17:24:59 +0900

    [05.09] book detail 수정 중

--- frontend/litlog/src/components/Book/BookList/BookList.jsx
+++ frontend/litlog/src/components/Book/BookList/BookList.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import './BookList.css';
+import styles from './BookList.module.css';
 
 function BookList({ books, onItemClick }) {
   return (
-    <div className="book-list">
+    <div className={styles['book-list']}>
       {books.map((book, index) => {
         const thumbnail = book.volumeInfo.imageLinks?.thumbnail || '/images/covernotavailable.png';
         return (
           <div key={book.id || book.volumeInfo.industryIdentifiers?.[0]?.identifier} 
-            className="book-item" onClick={()=>onItemClick(book.id)}>
-            <div className="index-box">
-              <p className="index-box-p">{index+1}</p>
+            className={styles['book-item']} 
+            onClick={() => onItemClick(book.id)}>
+            <div className={styles['index-box']}>
+              <p className={styles['index-box-p']}>{index + 1}</p>
             </div>
-            <div className="thumbnail">
+            <div className={styles['thumbnail']}>
               <img src={thumbnail} alt={book.volumeInfo.title} />
             </div>
-            <div className="book-info">
-              <h3 className="book-title">{book.volumeInfo.title}</h3>
-              {book.volumeInfo.subtitle && <p className="subtitle">{book.volumeInfo.subtitle}</p>}
-              <p className="authors">{book.volumeInfo.authors?.join(', ')}</p>
-              <p className="publisher">
+            <div className={styles['book-info']}>
+              <h3 className={styles['book-title']}>{book.volumeInfo.title}</h3>
+              {book.volumeInfo.subtitle && <p className={styles['subtitle']}>{book.volumeInfo.subtitle}</p>}
+              <p className={styles['authors']}>{book.volumeInfo.authors?.join(', ')}</p>
+              <p className={styles['publisher']}>
                 {book.volumeInfo.publisher} | {book.volumeInfo.publishedDate}
               </p>
             </div>
