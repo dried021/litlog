d6689e8658bcd77299cb9a2fbc1d6a731a0567c8
Seunghyun Nam <shyunam@gmail.com>
2025-05-11 22:19:58 +0900

    [05.11] book stack component 추가

---
+++ frontend/litlog/src/components/Profile/BookStack.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./ProfileSummary.module.css";
+
+export default function BookStack() {
+    return (
+        <div className={styles.bookStack}></div>
+    )
+}
