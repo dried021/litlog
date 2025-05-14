bf91dd0dbdc4e5f29e0d9be0613526fbb0820221
yunsunho <yunsh0613@skuniv.ac.kr>
2025-05-14 17:10:55 +0900

    collectionDetail

--- backend/src/main/java/com/bookfox/repository/CollectionMapper.java
+++ backend/src/main/java/com/bookfox/repository/CollectionMapper.java
@@ -3,6 +3,7 @@ package com.bookfox.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.bookfox.model.CollectionDto;
 
@@ -11,5 +12,6 @@ public interface CollectionMapper {
     List<CollectionDto> selectPopularCollectionsThisWeek();
     List<CollectionDto> selectAllCollectionsSortedByLikes();
     List<CollectionDto> selectAllCollectionsSortedByRecent();
+    List<CollectionDto> selectCollectionById(int id);
 }
 
