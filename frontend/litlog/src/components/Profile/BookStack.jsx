8a2246349b45ad288ef382edab189da6de3caf4f
Seunghyun Nam <shyunam@gmail.com>
2025-05-14 18:08:50 +0900

    [05.14] bookstack 시작

--- frontend/litlog/src/components/Profile/BookStack.jsx
+++ frontend/litlog/src/components/Profile/BookStack.jsx
@@ -1,8 +1,25 @@
 import React from "react"
-import styles from "./ProfileSummary.module.css";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import styles from "./BookStack.module.css";
 
 export default function BookStack() {
+    const {userId} = useParams();
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:9090/members/${userId}/bookshelf/finished`)
+            .then(res => res.json())
+            .then(data => setBooks(data))
+            .catch(error => console.error("Failed to fetch books", error));
+    }, [userId]);
+
     return (
-        <div className={styles.bookStack}></div>
+        <div className={styles.bookStack}>
+            <ul className={styles.bookList}>
+                
+            </ul>
+        </div>
     )
 }
