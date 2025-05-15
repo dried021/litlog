3aea7df30d237fc293a8c196b089ab640d36d276
Seunghyun Nam <shyunam@gmail.com>
2025-05-15 14:28:40 +0900

    [05.15] book stack 끝

--- frontend/litlog/src/components/Profile/BookStack.jsx
+++ frontend/litlog/src/components/Profile/BookStack.jsx
@@ -3,23 +3,44 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import styles from "./BookStack.module.css";
+import BookStackBook from "./BookStackBook";
+
+
+const colors = ["#636B2F", "#BAC095", "#D4DE95", "#3D4127"];
+
+function getNonRepeatingColor(prevColor) {
+    let newColor;
+    do {
+        newColor = colors[Math.floor(Math.random() * colors.length)];
+    } while (newColor === prevColor);
+    return newColor;
+}
 
 export default function BookStack() {
     const {userId} = useParams();
-    const [books, setBooks] = useState([]);
+    const [result, setResult] = useState({
+        "totalCount" : 0,
+        "books": []
+    });
 
     useEffect(() => {
         fetch(`http://localhost:9090/members/${userId}/bookshelf/finished`)
             .then(res => res.json())
-            .then(data => setBooks(data))
+            .then(data => setResult(data))
             .catch(error => console.error("Failed to fetch books", error));
     }, [userId]);
 
+
     return (
         <div className={styles.bookStack}>
-            <ul className={styles.bookList}>
-                
-            </ul>
+            {result.totalCount > 0 && <h2>{new Date().getFullYear()} BOOK STACK</h2>}
+            <div className={styles.bookList}>
+                {result.books.map(book => (
+                    <a href={`/books/${book.bookApiId}`} className={styles.bookLink}>
+                    <BookStackBook key={book.id} title={book.title.toUpperCase()} pageCount={book.pageCount}/>
+                    </a>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
