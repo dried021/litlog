9a3e723fc8372328c1b5fa53b291841ca3a6fb8c
dried021 <dried021@gmail.com>
2025-05-15 11:44:42 +0900

    [05.15] withdraw 완성

--- frontend/litlog/src/pages/Settings/Withdraw.jsx
+++ frontend/litlog/src/pages/Settings/Withdraw.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import styles from './Setting.module.css'; 
+import SideMenu from '../../components/Setting/SideMenu';
+import WithdrawUserForm from '../../components/Setting/WithdrawUserForm';
+
 
 const Withdraw = () => {
+    const [userId, setUserId] = useState("");
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    useEffect(()=>{
+        getUserId();
+    }, []);
+
+    const getUserId = async () => {
+        try {
+          const response = await axios.get(`http://localhost:9090/setting/user`);
+    
+          const {id, isAdmin} = response.data;
+          setUserId(id);
+          setIsAdmin(isAdmin);
+        } catch (error) {
+          console.error("Fail to load userId:", error);
+          setUserId("");
+        }
+      };
+
     return(
-        <div>
-            <h2> 탈퇴 페이지 </h2>
+        <>
+        <h2 className={styles.title}>Update My Info</h2>
+        <div className={styles.container}>
+            
+        <SideMenu isAdmin={isAdmin} />
+        <WithdrawUserForm userId={userId}/>
         </div>
+        </>
     );
 };
 
