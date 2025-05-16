980f6a99ab4321b08279226e7ba1997809f9fb99
Seunghyun Nam <shyunam@gmail.com>
2025-05-16 12:37:03 +0900

    [05.16] 회원 활동 백엔드 추가

---
+++ backend/src/main/java/com/bookfox/controller/mypage/ActivityController.java
@@ -0,0 +1,51 @@
+package com.bookfox.controller.mypage;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookfox.service.ActivityService;
+import com.bookfox.model.ActivityDto;
+
+import jakarta.annotation.Resource;
+
+@RestController
+@RequestMapping("activity")
+public class ActivityController {
+    @Resource
+    private ActivityService activityService;
+
+    @GetMapping("/following")
+    public ResponseEntity<Map<String,Object>> getFollowingActivityFeed(
+        @RequestParam String userId,
+        @RequestParam(defaultValue = "10") int limit,
+        @RequestParam(defaultValue = "0") int offset
+    ) {
+        Map<String, Object> response = new HashMap<>();
+        List<ActivityDto> result = activityService.getFollowingActivityFeed(userId, limit, offset);
+        
+        response.put("totalCount", result.size());
+        response.put("activities", result);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/following")
+    public ResponseEntity<Map<String,Object>> getIncomingActivityFeed(
+        @RequestParam String userId,
+        @RequestParam(defaultValue = "10") int limit,
+        @RequestParam(defaultValue = "0") int offset
+    ) {
+        Map<String, Object> response = new HashMap<>();
+        List<ActivityDto> result = activityService.getIncomingActivityFeed(userId, limit, offset);
+        
+        response.put("totalCount", result.size());
+        response.put("activities", result);
+        return ResponseEntity.ok(response);
+    }
+}
