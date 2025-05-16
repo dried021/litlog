7a3ab08cb12069f459c3a114a69358f84b6d6491
Seunghyun Nam <shyunam@gmail.com>
2025-05-16 12:37:03 +0900

    [05.16] 회원 활동 백엔드 추가

---
+++ backend/src/main/java/com/bookfox/model/ActivityDto.java
@@ -0,0 +1,27 @@
+package com.bookfox.model;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ActivityDto {
+    private String followUserId;
+    private String followUsername;
+    private String profileImage;
+
+    private String bookApiId;
+    private String bookTitle;
+
+    private int collectionId;
+    private String collectionTitle;
+
+    private int reviewId;
+    private int rating;
+    
+    private Timestamp creationDate;
+    private int activityType;
+    private String date;
+}
