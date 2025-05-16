21bbd1bfc3f84c148e2eed2439f3bca61da9f440
Seunghyun Nam <shyunam@gmail.com>
2025-05-17 08:32:46 +0900

    .

---
+++ frontend/litlog/src/pages/Admin/AdminMain.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import ManageUser from '../../components/Admin/ManageUser';
+import styles from './Admin.module.css'; 
+import AdminSideMenu from '../../components/Admin/AdminSideMenu';
+
+
+const AdminMain = () => {
+
+  return (
+    <>
+      <h2 className={styles.title}>Admin Dashboard</h2>
+      <div className={styles.container}>
+
+      <AdminSideMenu />
+      <ManageUser/>
+      </div>
+    </>
+  );
+};
+
+export default AdminMain;
