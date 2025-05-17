5fb6a60a8524b562b2da5bea0c2a7a42b117bcb9
yunsunho <yunsh0613@skuniv.ac.kr>
2025-05-18 04:09:19 +0900

    header

--- frontend/litlog/src/components/Header/Header.jsx
+++ frontend/litlog/src/components/Header/Header.jsx
@@ -1,23 +1,33 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../libs/UserContext';
+import styles from './Header.module.css';
 
 const Header = () => {
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
-    return (
-        <header>
-            <nav>
-                <Link to="/">LitLog</Link>
-                <Link to="/books">Books</Link>
-                <Link to="/collections">Collections</Link>
-                <Link to="/readers">Readers</Link>
-                <Link to="/sign-in">Sign in</Link>
-                <Link to={`/${user}`}>Profile</Link>
-                <Link to="/settings"> Setting </Link>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link to="/">LitLog</Link>
+      </div>
+      <nav className={styles.nav}>
+        <Link to="/books">Books</Link>
+        <Link to="/collections">Collections</Link>
+        <Link to="/readers">Readers</Link>
+      </nav>
+      <div className={styles.authMenu}>
+        {user ? (
+          <>
+            <Link to={`/${user}`}>Profile</Link>
+            <Link to="/settings">Settings</Link>
+          </>
+        ) : (
+          <Link to="/sign-in">Sign in</Link>
+        )}
+      </div>
+    </header>
+  );
+};
 
-}
 export default Header;
