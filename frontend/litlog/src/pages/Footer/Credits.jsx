da7be1dba44714a5cf82da44df56c5ab4c25a4f7
nlhyll <pnhmms@gmail.com>
2025-05-20 13:23:46 +0900

    [05.20] logout 수정 / Footer..ing

---
+++ frontend/litlog/src/pages/Footer/Credits.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './FooterPage.module.css';
+
+const Credits = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Credits</h2>
+      <p>This is the Credits page.</p>
+    </div>
+  );
+};
+
+export default Credits;
